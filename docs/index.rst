5938be11e47ad4a7e3387ec5b3b9fee7dc6a41b3
sofiapinto <anasbpinto3@gmail.com>
2023-09-13 12:20:48 +0100

    Setup Nesta Data Science cookiecutter

---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 1
+
+   api/asf_public_discourse_home_decarbonisation
+   License <license>
+
+asf_public_discourse_home_decarbonisation
+===============================
+
+Project to identify topics and patterns in home decarbonisation public discourse data.
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,26 @@
+"""asf_public_discourse_home_decarbonisation."""
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+BASE_DIR = Path(__file__).parent
+
+
+setup(
+    name="asf_public_discourse_home_decarbonisation",
+    long_description=open(BASE_DIR / "README.md").read(),
+    install_requires=read_lines(BASE_DIR / "requirements.txt"),
+    extras_require={"dev": read_lines(BASE_DIR / "requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Project to identify topics and patterns in home decarbonisation public discourse data.",
+    author="Nesta",
+    license="MIT",
+)
