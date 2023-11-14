80de4cd8f68d5029fd3a0466c31830ffbb2fdda7
sofiapinto <anasbpinto3@gmail.com>
2023-11-14 11:22:34 +0000

    script with text processing utils

---
+++ asf_public_discourse_home_decarbonisation/utils/text_processing_utils.py
@@ -0,0 +1,126 @@
+"""
+Utils for processing text.
+"""
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk import ngrams
+import nltk
+
+nltk.download("stopwords")
+from gensim.parsing.preprocessing import STOPWORDS
+import re
+import string
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tag import pos_tag
+
+
+def remove_urls(text: str) -> str:
+    """
+    Removes URLs from text.
+    Args:
+        text: a string, tipically one or multiple sentences long
+    Returns:
+        text without URLs
+    """
+    pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    cleaned_text = re.sub(pattern, " ", text)
+    return cleaned_text
+
+
+def replace_punctuation_with_space(text: str) -> str:
+    """
+    Replaces punctuation with space.
+    Args:
+        text: a string, tipically one or multiple sentences long
+    Returns:
+        text wihtout punctuation
+    """
+    pattern = r"[{}]".format(re.escape(string.punctuation))
+    replaced_text = re.sub(pattern, " ", text)
+    replaced_text = re.sub(r"\s+", " ", replaced_text)  # Remove consecutive spaces
+    return replaced_text.strip()  # Remove leading/trailing spaces
+
+
+def lemmatize_sentence(tokens: list) -> list:
+    """
+    Lemmatizes tokens according to appropriate Part of Speech.
+    Args:
+        tokens: a list of tokens
+    Returns:
+        A list of lemmatized tokens.
+    """
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = []
+    for word, tag in pos_tag(tokens):
+        if tag.startswith("NN"):
+            pos = "n"
+        elif tag.startswith("VB"):
+            pos = "v"
+        else:
+            pos = "a"
+        lemmatized_tokens.append(lemmatizer.lemmatize(word, pos))
+    return lemmatized_tokens
+
+
+def process_text(text: str) -> str:
+    """
+    Preprocesses text by:
+        - replacing &amp with "and"
+        - removing URLs
+        - puts text to lower case
+        - replaces punctuation with space
+        - applies lemmatisation
+
+    Args:
+        text:
+    Returns:
+        Preprocessed text.
+    """
+    text = re.sub("&amp;", " and ", text)
+    text = remove_urls(text)
+
+    text = text.lower()
+
+    text = replace_punctuation_with_space(text)
+
+    # Lemmatize
+    tokens = word_tokenize(text)
+    lemmatized_tokens = lemmatize_sentence(tokens)
+
+    # Join lemmatized tokens back into a single string
+    processed_text = " ".join(lemmatized_tokens)
+    return processed_text
+
+
+def create_ngram_frequencies(tokens: list, n: int) -> list:
+    """
+    Computes frequencies from n-grams.
+    Args:
+        tokens: a list of tokens
+        n: number of tokens considered for n-gram
+    Returns:
+        a list
+    """
+    # Generate n-grams (in this example)
+    n_grams = ngrams(tokens, n)
+
+    # Join the n-grams into strings
+    n_gram_strings = [" ".join(gram) for gram in n_grams]
+
+    return n_gram_strings
+
+
+def english_stopwords_definition() -> list:
+    """
+    Defines English stopwords by putting together NLTK and gensim stopwords.
+
+    Returns:
+        A list of English stopwords.
+    """
+    sw_nltk = stopwords.words("english")
+    sw_gensim = [s for s in STOPWORDS if s not in sw_nltk]
+
+    stopwords_list = sw_nltk + sw_gensim
+
+    return stopwords_list
