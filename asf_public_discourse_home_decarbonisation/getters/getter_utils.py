3f993d186da7e8e46c193af265ab056c99a3dc64
sofiapinto <anasbpinto3@gmail.com>
2023-11-15 15:21:47 +0000

    creating getter utils and updating getter functions

---
+++ asf_public_discourse_home_decarbonisation/getters/getter_utils.py
@@ -0,0 +1,85 @@
+from fnmatch import fnmatch
+import json
+import pickle
+import pandas as pd
+import boto3
+from typing import List
+
+from asf_public_discourse_home_decarbonisation import logger
+
+S3_BUCKET = "asf-public-discourse-home-decarbonisation"
+
+
+def get_s3_resource():
+    s3 = boto3.resource("s3")
+    return s3
+
+
+def load_s3_data(bucket_name, file_path):
+    """
+    Load data from S3 location.
+
+    bucket_name: The S3 bucket name
+    file_path: S3 key to load
+    """
+    s3 = get_s3_resource()
+
+    obj = s3.Object(bucket_name, file_path)
+    if fnmatch(file_path, "*.json"):
+        file = obj.get()["Body"].read().decode()
+        return json.loads(file)
+    elif fnmatch(file_path, "*.csv"):
+        return pd.read_csv("s3://" + bucket_name + "/" + file_path)
+    elif fnmatch(file_path, "*.parquet"):
+        return pd.read_parquet("s3://" + bucket_name + "/" + file_path)
+    elif fnmatch(file_path, "*.pkl") or fnmatch(file_path, "*.pickle"):
+        file = obj.get()["Body"].read().decode()
+        return pickle.loads(file)
+    else:
+        logger.error(
+            'Function not supported for file type other than "*.csv", "*.json", "*.pkl" or "*.parquet"'
+        )
+
+
+def fetch_file_paths_from_s3_folder(path_folder: str) -> List[str]:
+    """
+    Fetches file paths from a specified S3 folder.
+    Args:
+        path_folder (str): The path to the folder in S3.
+
+    Returns:
+        List[str]: list of strings with the file paths.
+    """
+    # Create an S3 client
+    s3_client = boto3.client("s3")
+
+    objects_in_folder = s3_client.list_objects_v2(
+        Bucket=S3_BUCKET,
+        Prefix=path_folder,
+    )
+
+    # Extract the filepaths from the response
+    all_object_keys = [obj["Key"] for obj in objects_in_folder.get("Contents", [])]
+
+    next_marker = objects_in_folder.get("NextContinuationToken")
+
+    while next_marker:
+        # List objects in the specified folder
+        objects_in_folder = s3_client.list_objects_v2(
+            Bucket=S3_BUCKET,
+            Prefix=path_folder,
+            ContinuationToken=next_marker,
+        )
+
+        # Extract the filepaths from the response
+        object_keys = [obj["Key"] for obj in objects_in_folder.get("Contents", [])]
+
+        all_object_keys.extend(object_keys)
+
+        # Check if there are more objects to retrieve
+        next_marker = objects_in_folder.get("NextContinuationToken")
+
+    # Removing the paths to folders, leaving only the paths to files ending with ".parquet"
+    file_paths = [fp for fp in all_object_keys if fp.endswith(".parquet")]
+
+    return file_paths
--- asf_public_discourse_home_decarbonisation/getters/mse_getters.py
+++ asf_public_discourse_home_decarbonisation/getters/mse_getters.py
@@ -2,20 +2,43 @@
 Getter functions for MSE data.
 """
 import pandas as pd
-from nesta_ds_utils.loading_saving import S3
-from asf_public_discourse_home_decarbonisation import base_config
+from asf_public_discourse_home_decarbonisation.getters.getter_utils import (
+    load_s3_data,
+    fetch_file_paths_from_s3_folder,
+)
 
-S3_BUCKET = base_config["S3_BUCKET"]
+S3_BUCKET = "asf-public-discourse-home-decarbonisation"
 
 
 def get_first_attempt_mse_data() -> pd.DataFrame:
     """
-    Returns a dataframe with 10 pages of MSE data
-    from the Green Ethical Money Saving category.
+    Returns a dataframe with 10 pages of MSE data from the Green Ethical Money Saving category.
     """
-    return S3.download_obj(
-        bucket=S3_BUCKET,
-        path_from="data/mse/first_attempt/green-ethical-moneysaving.parquet",
-        download_as="dataframe",
-        kwargs_reading={"engine": "python"},
+    return load_s3_data(
+        bucket_name=S3_BUCKET,
+        file_path="data/mse/first_attempt/green-ethical-moneysaving.parquet",
     )
+
+
+def get_mse_category_data(category: str) -> pd.DataFrame:
+    """
+    Returns a dataframe with most up to date category/sub-forum data
+    """
+    return load_s3_data(
+        bucket_name=S3_BUCKET,
+        file_path=f"data/mse/outputs/mse_data_category_{category}.parquet",
+    )
+
+
+def get_all_mse_data() -> pd.DataFrame:
+    """
+    Returns a dataframe with data from all categories
+    """
+    file_paths = fetch_file_paths_from_s3_folder("data/mse/outputs/")
+
+    all_mse_data = pd.DataFrame()
+    for fp in file_paths:
+        aux = load_s3_data(bucket_name=S3_BUCKET, file_path=fp)
+        all_mse_data = pd.concat([all_mse_data, aux])
+
+    return all_mse_data.drop_duplicates().reset_index(drop=True)
