1094547bdf9d1fea108cf4c725e0b9e00a96a253
sofiapinto <anasbpinto3@gmail.com>
2023-12-22 13:04:02 +0000

    updating text processing flow

--- asf_public_discourse_home_decarbonisation/pipeline/data_processing_flows/flow_utils.py
+++ asf_public_discourse_home_decarbonisation/pipeline/data_processing_flows/flow_utils.py
@@ -1,161 +1,26 @@
 """
-Util functions.
+Text processing functions to:
+- remove URLs from text
+- remove username patterns;
+- remove specific tokens from text;
+- define english stopwords using NLTK and spaCy;
+- lemmatise text;
+- tokenise text;
 """
 
 import pandas as pd
-import boto3
-import sys
 import logging
-from fnmatch import fnmatch
-import json
-import pickle
-import boto3
-from typing import List, Union, Dict
+from typing import Union
+import re
+from nltk.corpus import stopwords
 import spacy
 
 # Load the English language model
 nlp = spacy.load("en_core_web_sm")
-import re
-from nltk.corpus import stopwords
-import nltk
-
-nltk.download("stopwords")
-from gensim.parsing.preprocessing import STOPWORDS
 
 logger = logging.getLogger(__name__)
 
 S3_BUCKET = "asf-public-discourse-home-decarbonisation"
-
-
-def get_s3_resource():
-    s3 = boto3.resource("s3")
-    return s3
-
-
-def load_s3_data(bucket_name: str, file_path: str) -> Union[pd.DataFrame, Dict]:
-    """
-    Load data from S3 location.
-
-    Args:
-        bucket_name (str) : The S3 bucket name
-        file_path (str): S3 key to load
-    Returns:
-        Union[pd.DataFrame, Dict]: the loaded data
-    """
-    s3 = get_s3_resource()
-
-    obj = s3.Object(bucket_name, file_path)
-    if fnmatch(file_path, "*.json"):
-        file = obj.get()["Body"].read().decode()
-        return json.loads(file)
-    elif fnmatch(file_path, "*.csv"):
-        return pd.read_csv("s3://" + bucket_name + "/" + file_path)
-    elif fnmatch(file_path, "*.parquet"):
-        return pd.read_parquet("s3://" + bucket_name + "/" + file_path)
-    elif fnmatch(file_path, "*.pkl") or fnmatch(file_path, "*.pickle"):
-        file = obj.get()["Body"].read().decode()
-        return pickle.loads(file)
-    else:
-        logger.error(
-            'Function not supported for file type other than "*.csv", "*.json", "*.pkl" or "*.parquet"'
-        )
-
-
-def get_mse_category_data(category: str, collection_date: str) -> pd.DataFrame:
-    """
-    Gets data from a specific Money Saving Expert category, collected on a certain date.
-
-    Args:
-        category (str): An MSE category
-        collection_date (str): A date in the format "YYYY_MM_DD"
-    Returns:
-        pd.DataFramedataframe with most up to date category/sub-forum data
-    """
-    if category == "energy":
-        return load_s3_data(
-            bucket_name=S3_BUCKET,
-            file_path=f"data/mse/outputs/mse_data_category_energy.parquet",
-        )
-
-    return load_s3_data(
-        bucket_name=S3_BUCKET,
-        file_path=f"data/mse/outputs/mse_data_category_{category}_{collection_date}.parquet",
-    )
-
-
-def get_all_mse_data(collection_date: str) -> pd.DataFrame:
-    """
-    Gets data from all Money Saving Expert categories.
-
-    Args:
-        collection_date (str): A date in the format "YYYY_MM_DD"
-    Returns:
-        pd.DataFrame: a dataframe with data from all categories
-    """
-    categories = [
-        "green-ethical-moneysaving",
-        "lpg-heating-oil-solid-other-fuels",
-        "energy",
-        "is-this-quote-fair",
-    ]
-    all_mse_data = pd.DataFrame()
-    for cat in categories:
-        if (
-            cat != "energy"
-        ):  # this is a temporary fix, while we sort the collection for this category
-            aux = load_s3_data(
-                bucket_name=S3_BUCKET,
-                file_path=f"data/mse/outputs/mse_data_category_{cat}_{collection_date}.parquet",
-            )
-        else:
-            aux = load_s3_data(
-                bucket_name=S3_BUCKET,
-                file_path=f"data/mse/outputs/mse_data_category_energy.parquet",
-            )
-        all_mse_data = pd.concat([all_mse_data, aux])
-
-    return all_mse_data.drop_duplicates().reset_index(drop=True)
-
-
-def get_mse_data(category: str, collection_datetime: str) -> pd.DataFrame:
-    """
-    Get MSE data for a specific category and collection date.
-    Existing categories are: "green-ethical-moneysaving", "lpg-heating-oil-solid-other-fuels", "energy", "is-this-quote-fair"
-    Additionally, you can also use "sample" (to get the initial sample collected) or "all" (to get all the MSE data collected).
-
-    Args:
-        category (str): An MSE category, "sample" or "all"
-        collection_datetime (str): A date in the format "YYYY_MM_DD"
-
-    Returns:
-        pd.DataDrame: a dataframe with the MSE data
-    """
-    mse_categories = [
-        "green-ethical-moneysaving",
-        "lpg-heating-oil-solid-other-fuels",
-        "energy",
-        "is-this-quote-fair",
-    ]
-
-    accepted_categories = mse_categories + ["all"]
-    try:
-        # Check if the category is in the list if one of the accepted categories
-        category_index = accepted_categories.index(category)
-        if category in [
-            "green-ethical-moneysaving",
-            "lpg-heating-oil-solid-other-fuels",
-            "is-this-quote-fair",
-            "energy",
-        ]:
-            mse_data = get_mse_category_data(category, collection_datetime)
-        else:  # category == "all"
-            mse_data = get_all_mse_data(collection_datetime)
-        logger.info(f"Data for category '{category}' imported successfully from S3.")
-    except ValueError:
-        logger.error(f"{category} is not a valid category!")
-        sys.exit(-1)
-
-    return mse_data
 
 
 def remove_urls(text: str) -> str:
@@ -166,8 +31,11 @@ def remove_urls(text: str) -> str:
     Returns:
         str: text without URLs
     """
-    pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-    cleaned_text = re.sub(pattern, " ", text)
+    # Define a regular expression pattern for matching URLs
+    url_pattern = re.compile(r"https?://\S+|www\.\S+")
+
+    # Replace URLs with a space
+    cleaned_text = url_pattern.sub(" ", text)
     return cleaned_text
 
 
@@ -186,54 +54,39 @@ def remove_text_after_patterns(text: str) -> str:
     return result
 
 
-def remove_items_in_list(mse_data: pd.DataFrame, list_of_items: list) -> pd.DataFrame:
+def remove_tokens_in_list(
+    data: Union[pd.Series, list], list_of_tokens_to_remove: list
+) -> Union[pd.Series, list]:
     """
-    Removes items from text and titles.
+    Removes specific tokens from a pandas Series or list.
 
     Args:
-        mse_data (pd.DataFrame): Money Saving Expert data
-        list_of_items (list): list of items to be removed
-    Returns:
-        pd.DataFrame: MSE data with items removed from text and titles
-    """
-    mse_data["tokens_title"] = mse_data["tokens_title"].apply(
-        lambda x: [token for token in x if token not in list_of_items]
-    )
-    mse_data["tokens_text"] = mse_data["tokens_text"].apply(
-        lambda x: [token for token in x if token not in list_of_items]
-    )
-    return mse_data
-
-
-def english_stopwords_definition() -> list:
-    """
-    Defines English stopwords by putting together NLTK and gensim stopwords.
+        data (Union[pd.Series, list]): original data
+        list_of_tokens_to_remove (list): list of tokens to be removed
 
     Returns:
-        list: a list of English stopwords.
+        Union[pd.Series, list]: data without tokens in list
     """
-    sw_nltk = stopwords.words("english")
-    sw_gensim = [s for s in STOPWORDS if s not in sw_nltk]
-
-    stopwords_list = sw_nltk + sw_gensim
-
-    return stopwords_list
+    return [token for token in data if token not in list_of_tokens_to_remove]
 
 
-def process_text(text: str) -> str:
+def preprocess_text(text: str) -> str:
     """
     Preprocesses text by:
         - replacing &amp with "and"
+        - replacing & with "and"
         - removing URLs
         - puts text to lower case
         - removing username patterns
 
     Args:
-        text:
+        text (str): text to be processed
     Returns:
-        Preprocessed text.
+        str: Preprocessed text.
     """
     text = re.sub("&amp;", " and ", text)
+    text = re.sub("&", " and ", text)
+
     text = remove_urls(text)
 
     text = text.lower()
@@ -243,25 +96,54 @@ def process_text(text: str) -> str:
     return text
 
 
+def english_stopwords_definition() -> list:
+    """
+    Defines English stopwords by putting together NLTK and Spacy stopwords.
+
+    Returns:
+        list: a list of English stopwords.
+    """
+    spacy_stopwords = list(spacy.lang.en.STOP_WORDS)
+    nltk_stopwrods = stopwords.words("english")
+
+    return list(set(spacy_stopwords + nltk_stopwrods))
+
+
 def lemmatise(text: str) -> list:
     """
     Applies lemmatisation sentence by sentence.
-    It then removes punctuations, stopwords and tokens tagged as space (e.g. "\n")
+    It then removes punctuations and tokens tagged as space (e.g. "\n")
 
     Args:
-        text (str): any text
+        text (str): text to be lemmatise, might include multiple sentences
 
     Returns:
-        list: list of lemmatised token without stopwords or punctuation
+        list: list of lemmatised token without punctuation tokens
     """
     doc = nlp(text)
     # apply sentence by sentence lemmatisation
-    processed_sentences = []
+    processed_sentence_tokens = []
     for sentence in doc.sents:
-        lemmatized_tokens = [
+        lemmatised_tokens = [
             token.lemma_
             for token in sentence
-            if not (token.is_punct or token.is_stop or token.pos_ == "SPACE")
+            if not (token.is_punct or token.pos_ == "SPACE")
         ]
-        processed_sentences.extend(lemmatized_tokens)
-    return processed_sentences
+        processed_sentence_tokens.extend(lemmatised_tokens)
+    return processed_sentence_tokens
+
+
+def tokenise(text: str) -> list:
+    """
+    Applies tokenisation and removes punctuation and tokens tagged as space (e.g. "\n")
+
+    Args:
+        text (str): text to be tokenised, might include multiple sentences
+
+    Returns:
+        list: list of tokens without punctuation tokens
+    """
+    doc = nlp(text)
+    return [
+        token.text for token in doc if not (token.is_punct or token.pos_ == "SPACE")
+    ]
--- asf_public_discourse_home_decarbonisation/pipeline/data_processing_flows/processing_text_data.py
+++ asf_public_discourse_home_decarbonisation/pipeline/data_processing_flows/processing_text_data.py
@@ -13,22 +13,46 @@ os.system(
 os.system("python -m spacy download en_core_web_sm")
 
 from metaflow import FlowSpec, step, batch, Parameter
+import pandas as pd
+from flow_utils import remove_tokens_in_list
 
 S3_BUCKET = "asf-public-discourse-home-decarbonisation"
+MSE_S3_RAW_OUTPUTS_FOLDER_PATH = "data/mse/outputs"
 MSE_S3_PROCESSED_OUTPUTS_FOLDER_PATH = "data/mse/outputs/processed"
 CHUNKSIZE = 10000
 
 
+def remove_tokens_from_specified_columns(
+    data: pd.DataFrame, columns: list, list_of_tokens_to_remove: set
+) -> pd.DataFrame:
+    """
+    Removes items in a list provided from certain columns in data.
+
+    Args:
+        data (pd.DataFrame): data
+        columns (list): column names
+        list_of_tokens_to_remove (list): list of items to be removed
+    Returns:
+        pd.DataFrame: MSE data with items removed from text and titles
+    """
+    for col in columns:
+        data[col + "_no_stopwords"] = data.apply(
+            lambda x: remove_tokens_in_list(x[col], list_of_tokens_to_remove), axis=1
+        )
+
+    return data
+
+
 class TextProcessingFlow(FlowSpec):
     category = Parameter(
-        name="mse_category",
-        help="An MSE category or sub-forum",
+        name="category",
+        help="A category or sub-forum",
         default="green-ethical-moneysaving",
     )
 
     batch_date = Parameter(
         name="batch",
-        help='Batch name (e.g. datetime of collection "2023_11_15" or "newest" for the most up to date)',
+        help='Batch date (e.g. date of collection "2023_11_15" or "newest" for the most up to date version of the data)',
         default="2023_11_15",
     )
 
@@ -41,29 +65,33 @@ class TextProcessingFlow(FlowSpec):
     @step
     def start(self):
         """
-        Starts the flow.
+        Starts the flow and reads raw data from S3.
         """
-        from flow_utils import (
-            get_mse_data,
-        )
+        from smart_open import open
+        import pandas as pd
 
-        self.mse_data = get_mse_data(self.category, self.batch_date)
+        s3_path = f"s3://{S3_BUCKET}/{MSE_S3_RAW_OUTPUTS_FOLDER_PATH}/mse_data_category_{self.category}_{self.batch_date}.parquet"
+        with open(s3_path, "rb") as s3_file:
+            self.mse_data = pd.read_parquet(s3_file)
+
+        if self.test:  # working with less data if just testing
+            self.mse_data = self.mse_data[:100]
 
         self.next(self.process_text_and_titles)
 
     @step
     def process_text_and_titles(self):
         """
-        Pre-processing text and titles before applying lemmatisation by
+        Pre-processing text and titles (before applying lemmatisation) by
         removing URLs, putting text to lowercase and removing username patterns.
         """
-        from flow_utils import process_text
+        from flow_utils import preprocess_text
 
         self.mse_data["processed_text"] = self.mse_data["text"].apply(
-            lambda x: process_text(x)
+            lambda x: preprocess_text(x)
         )
         self.mse_data["processed_title"] = self.mse_data["title"].apply(
-            lambda x: process_text(x)
+            lambda x: preprocess_text(x)
         )
 
         self.next(self.prepare_for_lemmatising)
@@ -78,32 +106,43 @@ class TextProcessingFlow(FlowSpec):
             self.mse_data[i : i + CHUNKSIZE]
             for i in range(0, len(self.mse_data) + 1, CHUNKSIZE)
         ]
-        self.next(self.lemmatising_text_data, foreach="chunks")
+        self.next(self.lemmatising_and_tokenise_text_data, foreach="chunks")
 
     @batch(cpu=2, memory=8000)
     @step
-    def lemmatising_text_data(self):
+    def lemmatising_and_tokenise_text_data(self):
         """
-        Lemmatising titles and text from posts and replies.
-        Because titles are the same for all replies to a post, we only lemmatise unique titles.
+        Creates columns with lemmatising and tokenised titles and text from posts and replies.
+        Because titles are the same for all replies to a post, we only lemmatise/tokenise unique titles.
         """
-        from flow_utils import lemmatise
+        from flow_utils import lemmatise, tokenise
 
         data = self.input
 
         # Start by lemmatising unique titles
         titles = data[["processed_title", "id"]]
         titles.drop_duplicates("id", inplace=True)
+
         # Mapping original titles to the lemmatised titles in the original data
-        titles["processed_title"] = titles["processed_title"].apply(lemmatise)
+        titles["lemmatised_tokens_title"] = titles["processed_title"].apply(lemmatise)
+        titles["tokens_title"] = titles["processed_title"].apply(tokenise)
+
+        # Setting the same index in both dataframes
         titles.set_index("id", inplace=True)
         data.set_index("id", inplace=True)
-        data["tokens_title"] = titles["processed_title"]
+
+        # Assigning the lemmatised title according to ID
+        data["lemmatised_tokens_title"] = titles["lemmatised_tokens_title"]
+        data["tokens_title"] = titles["tokens_title"]
+
+        # Reseting the dataset
         data.reset_index(inplace=True)
 
         # Lemmatising text from posts and replies
-        data["tokens_text"] = data["processed_text"].apply(lemmatise)
+        data["lemmatised_tokens_text"] = data["processed_text"].apply(lemmatise)
+        data["tokens_text"] = data["processed_text"].apply(tokenise)
 
+        # Passing variable data to the next step using self
         self.data = data
 
         self.next(self.join_data_from_previous_step)
@@ -120,28 +159,33 @@ class TextProcessingFlow(FlowSpec):
         for input in inputs:
             self.mse_data = pd.concat([self.mse_data, input.data])
 
-        self.next(self.remove_stopwords_and_punctuation)
+        self.next(self.remove_stopwords)
 
     @step
-    def remove_stopwords_and_punctuation(self):
+    def remove_stopwords(self):
         """
         Removing stopwords and punctuation.
         """
-        from flow_utils import remove_items_in_list, english_stopwords_definition
-        import string
+        from flow_utils import english_stopwords_definition
 
         stopwords = english_stopwords_definition()
-        self.mse_data = remove_items_in_list(self.mse_data, stopwords)
-
-        self.mse_data = remove_items_in_list(self.mse_data, string.punctuation)
-        self.mse_data = remove_items_in_list(self.mse_data, ["="])
+        self.mse_data = remove_tokens_from_specified_columns(
+            self.mse_data,
+            [
+                "tokens_text",
+                "tokens_title",
+                "lemmatised_tokens_title",
+                "lemmatised_tokens_text",
+            ],
+            stopwords,
+        )
 
         self.next(self.save_data)
 
     @step
     def save_data(self):
         """
-        Saving data.
+        Saving data to S3.
         """
         if not self.test:  # only saves data if not in test mode
             self.mse_data.to_parquet(
