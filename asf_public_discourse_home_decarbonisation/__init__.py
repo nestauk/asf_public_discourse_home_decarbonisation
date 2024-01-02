46e69b8532de536d949636d9bd081f157a481efe
sofiapinto <anasbpinto3@gmail.com>
2024-01-02 17:45:55 +0000

    Moving S3 bucket definition

--- asf_public_discourse_home_decarbonisation/__init__.py
+++ asf_public_discourse_home_decarbonisation/__init__.py
@@ -17,6 +17,9 @@ def get_yaml_config(file_path: Path) -> Optional[dict]:
 # Define project base directory
 PROJECT_DIR = Path(__file__).resolve().parents[1]
 
+# S3 bucket name
+S3_BUCKET = "asf-public-discourse-home-decarbonisation"
+
 # Define log output locations
 info_out = str(PROJECT_DIR / "info.log")
 error_out = str(PROJECT_DIR / "errors.log")
