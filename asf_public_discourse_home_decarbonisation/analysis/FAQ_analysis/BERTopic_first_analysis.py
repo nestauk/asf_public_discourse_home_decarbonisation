0c7574e6b00eb2ed73e4fddf6e786d4ab87dce17
helloaidank <helloaidank@gmail.com>
2024-02-20 12:47:55 +0000

    added documentation and modularisation

--- asf_public_discourse_home_decarbonisation/analysis/FAQ_analysis/BERTopic_first_analysis.py
+++ asf_public_discourse_home_decarbonisation/analysis/FAQ_analysis/BERTopic_first_analysis.py
@@ -1,6 +1,20 @@
+"""
+python BERTopic_first_analysis.py
+
+This script performs topic modeling on a set of questions using the BERTopic library.
+
+The process includes the following steps:
+1. Load the 'extracted questions' data from a CSV file, extracting the 'Question' column.
+2. Create a BERTopic model and fit it to the questions.
+3. Visualise the topics identified by the model in various ways, including a general topic visualization, a bar chart of the top topics, and a hierarchy of the topics.
+4. Plot the distribution of topics.
+
+The script is designed to be modular, with separate functions for each step of the process. This makes the code easier to read and maintain, and allows for parts of the code to be reused elsewhere if needed.
+"""
 import pandas as pd
 from bertopic import BERTopic
 import matplotlib.pyplot as plt
+import argparse
 from asf_public_discourse_home_decarbonisation.config.plotting_configs import (
     set_plotting_styles,
     NESTA_COLOURS,
@@ -8,80 +22,148 @@ from asf_public_discourse_home_decarbonisation.config.plotting_configs import (
 from asf_public_discourse_home_decarbonisation.utils.plotting_utils import (
     finding_path_to_font,
 )
+from asf_public_discourse_home_decarbonisation import PROJECT_DIR
+from typing import List, Tuple
+import os
 
 set_plotting_styles()
 font_path_ttf = finding_path_to_font("Averta-Regular")
 
-# Load the CSV file
-# file_path = '../forum_120_ground_source_heat_pumps_gshp/extracted_questions_120_ground_source_heat_pumps_gshp.csv'  # Make sure to replace this with your actual file path
-file_path = "/Users/aidan.kelly/nesta/ASF/asf_public_discourse_home_decarbonisation/outputs/extracted_questions/bh/forum_119_air_source_heat_pumps_ashp/extracted_questions_119_air_source_heat_pumps_ashp.csv"
-df = pd.read_csv(file_path)
 
-# Assuming your questions are in a column named 'questions'
-questions = df["Question"].tolist()
+def create_argparser() -> argparse.ArgumentParser:
+    """
+    Creates an argument parser that can receive the following arguments:
+    - category: category or sub-forum (defaults to "119_air_source_heat_pumps_ashp")
+    - forum: forum (i.e. mse or bh) (defaults to "bh")
+    Returns:
+        argparse.ArgumentParser: argument parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--category",
+        help="Category or sub-forum",
+        default="119_air_source_heat_pumps_ashp",
+        type=str,
+    )
 
-# Initialize BERTopic
-topic_model = BERTopic()
+    parser.add_argument(
+        "--forum",
+        help="forum (i.e. mse or bh)",
+        default="bh",
+        type=str,
+    )
+
+    args = parser.parse_args()
+    return args
 
 
-# Fit the model on your questions
-topics, probabilities = topic_model.fit_transform(questions)
+def load_data(file_path: str) -> List[str]:
+    """
+    Loads extracted questions from a CSV file into a list.
 
-fig = topic_model.visualize_topics()
-# Save the figure as a png file
-fig.write_image("topic_visualization.png")
+    Args:
+        file_path (str): The path to the CSV file containing the questions.
 
-fig_barchart = topic_model.visualize_barchart(top_n_topics=16, n_words=10)
-"""
-for trace in fig_barchart.data:
-    trace.marker.color = NESTA_COLOURS[0]  # Set each bar's color to NESTAS[0]
-fig_barchart.show()
+    Returns:
+        List[str]: A list of questions.
+    """
+    return pd.read_csv(file_path)["Question"].tolist()
 
-plt.show()
-"""
-fig_barchart.write_image("topic_visualization_barchart.png")
 
-# fig_hierarchy = topic_model.visualize_hierarchy()
+def create_topic_model(questions: List[str]) -> Tuple[BERTopic, List[int], List[float]]:
+    """
+    Creates and fits a BERTopic model to the given list of questions.
 
-# fig_hierarchy.write_image("topic_visualization_hierarchy.png")
+    Args:
+        questions (List[str]): A list of questions to model.
 
-# Print the topics found
-print(topic_model.get_topic_info())  # This prints the basic info about the topics found
+    Returns:
+        tuple: Contains the fitted BERTopic model, topics, and their probabilities.
+    """
+    topic_model = BERTopic()
+    topics, probabilities = topic_model.fit_transform(questions)
+    return topic_model, topics, probabilities
 
-topics_info = topic_model.get_topic_info()
-topics_info["%"] = topics_info["Count"] / len(questions) * 100
-# topics_info
-# doc_info = topic_model.get_document_info(questions)
-# doc_info
 
-"""
+def visualise_topics(topic_model: BERTopic, figure_file_path: str) -> None:
+    """
+    Generates and saves a visualisation of topics identified by the BERTopic model.
 
-for i in range(10):  # Adjust the range as needed
-    topic = topics_info.iloc[i]
-    print(f"Topic {topic['Topic']} - Count: {topic['Count']}")
-    print(f"Name: {topic['Name']}")
-    print(f"Representation: {topic['Representation']}")
-    print(f"Representative Docs: {topic['Representative_Docs']}")
-    print()
-"""
-# Topic Distribution Bar Chart
-topic_counts = topic_model.get_topic_info()["Count"][1:17]
-topic_labels = topic_model.get_topic_info()["Name"][1:17].str.replace("_", " ")
-plt.figure(figsize=(14, 8))
-plt.barh(topic_labels, topic_counts, color=NESTA_COLOURS[0])
-# plt.barh(topic_labels, topic_counts)
-# plt.xticks(rotation=90)
-plt.ylabel("Topics")
-plt.xlabel("Count")
-plt.title("Topic Distribution")
-plt.tight_layout()
-# Save the figure before showing it
-plt.savefig("topic_distribution.png", dpi=300, bbox_inches="tight")
-plt.show()
+    Args:
+        topic_model (BERTopic): The BERTopic model after fitting to data.
+    """
+    fig = topic_model.visualize_topics()
+    fig.write_image(figure_file_path + "topic_visualisation.png")
 
-# new_topics, new_probs = topic_model.reduce_topics(questions, nr_topics=30)
 
-# Now visualize the hierarchy with the reduced number of topics
-fig_hierarchy = topic_model.visualize_hierarchy()
+def visualise_barchart(topic_model: BERTopic, figure_file_path: str) -> None:
+    """
+    Generates and saves a barchart visualisation of the top n topics identified by the BERTopic model.
 
-fig_hierarchy.write_image("topic_visualization_hierarchy.png")
+    Args:
+        topic_model (BERTopic): The BERTopic model after fitting to data.
+    """
+    fig_barchart = topic_model.visualize_barchart(top_n_topics=16, n_words=10)
+    fig_barchart.write_image(figure_file_path + "topic_visualisation_barchart.png")
+
+
+def visualise_hierarchy(topic_model: BERTopic, figure_file_path: str) -> None:
+    """
+    Generates and saves a hierarchical visualisation of topics identified by the BERTopic model.
+
+    Args:
+        topic_model (BERTopic): The BERTopic model after fitting to data.
+    """
+    fig_hierarchy = topic_model.visualize_hierarchy()
+    fig_hierarchy.write_image(figure_file_path + "topic_visualisation_hierarchy.png")
+
+
+def plot_topic_distribution(topic_model: BERTopic, figure_file_path: str) -> None:
+    """
+    Plots and saves the distribution of the top topics identified by the BERTopic model.
+
+    Args:
+        topic_model (BERTopic): The BERTopic model after fitting to data.
+    """
+    topic_counts = topic_model.get_topic_info()["Count"][1:17]
+    topic_labels = topic_model.get_topic_info()["Name"][1:17].str.replace("_", " ")
+    plt.figure(figsize=(14, 8))
+    plt.barh(topic_labels, topic_counts, color=NESTA_COLOURS[0])
+    plt.ylabel("Topics")
+    plt.xlabel("Count")
+    plt.title("Topic Distribution")
+    plt.tight_layout()
+    plt.savefig(
+        figure_file_path + "topic_distribution.png", dpi=300, bbox_inches="tight"
+    )
+
+
+def main():
+    """
+    Main function to execute the topic modeling workflow.
+
+    This function sets up the plotting styles, loads the data, creates a topic model from the questions,
+    and generates visualisations for topics, barchart, hierarchy, and topic distribution.
+    """
+    set_plotting_styles()
+    args = create_argparser()
+    category = args.category
+    forum = args.forum
+    input_data = os.path.join(
+        PROJECT_DIR,
+        f"outputs/data/extracted_questions/{forum}/forum_{category}/extracted_questions_{category}_all.csv",
+    )
+    figure_path = os.path.join(
+        PROJECT_DIR, f"outputs/figures/extracted_questions/{forum}/forum_{category}/"
+    )
+    os.makedirs(figure_path, exist_ok=True)
+    questions = load_data(input_data)
+    topic_model, topics, probabilities = create_topic_model(questions)
+    visualise_topics(topic_model, figure_path)
+    visualise_barchart(topic_model, figure_path)
+    visualise_hierarchy(topic_model, figure_path)
+    plot_topic_distribution(topic_model, figure_path)
+
+
+if __name__ == "__main__":
+    main()
--- asf_public_discourse_home_decarbonisation/analysis/FAQ_analysis/questions_eda_analysis.py
+++ asf_public_discourse_home_decarbonisation/analysis/FAQ_analysis/questions_eda_analysis.py
@@ -1,3 +1,15 @@
+"""
+This script performs exploratory data analysis (EDA) on a set of questions and can be run with following command:
+    'python questions_eda_analysis.py'
+
+The process includes the following steps:
+1. Load the data from a CSV file, extracting the 'Question' column.
+2. Count the frequency of each question.
+3. Wrap the text of each question and add an ellipsis if it exceeds a certain length.
+4. Plot the counts of the most frequent questions.
+
+The script is designed to be modular, with separate functions for each step of the process. This makes the code easier to read and maintain, and allows for parts of the code to be reused elsewhere if needed.
+"""
 from asf_public_discourse_home_decarbonisation.config.plotting_configs import (
     set_plotting_styles,
     NESTA_COLOURS,
@@ -5,34 +17,87 @@ from asf_public_discourse_home_decarbonisation.config.plotting_configs import (
 from asf_public_discourse_home_decarbonisation.utils.plotting_utils import (
     finding_path_to_font,
 )
-
+import os
+import pandas as pd
 import matplotlib.pyplot as plt
+import textwrap
 from matplotlib.ticker import MaxNLocator
+import argparse
+from asf_public_discourse_home_decarbonisation.config.plotting_configs import (
+    NESTA_COLOURS,
+)
+from asf_public_discourse_home_decarbonisation import PROJECT_DIR
 
 set_plotting_styles()
 font_path_ttf = finding_path_to_font("Averta-Regular")
 
-import pandas as pd
-import textwrap
 
-# Replace 'your_file.csv' with the path to your actual file
-# Replace 'question_column' with the actual name of the column containing questions
-# df = pd.read_csv('/Users/aidan.kelly/nesta/ASF/asf_public_discourse_home_decarbonisation/outputs/extracted_questions/bh/forum_119_air_source_heat_pumps_ashp/extracted_questions_119_air_source_heat_pumps_ashp.csv')
-df = pd.read_csv(
-    "/Users/aidan.kelly/nesta/ASF/asf_public_discourse_home_decarbonisation/outputs/extracted_questions/bh/forum_combined_data/extracted_questions_combined_data.csv"
-)
-# df = pd.read_csv('/Users/aidan.kelly/nesta/ASF/asf_public_discourse_home_decarbonisation/outputs/extracted_questions/bh/forum_combined_data/idk_phrases_combined_data.csv')
-questions = df["Question"]
-question_counts = questions.value_counts()
+def create_argparser() -> argparse.ArgumentParser:
+    """
+    Creates an argument parser that can receive the following arguments:
+    - category: category or sub-forum (defaults to "119_air_source_heat_pumps_ashp")
+    - forum: forum (i.e. mse or bh) (defaults to "bh")
+    Returns:
+        argparse.ArgumentParser: argument parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--category",
+        help="Category or sub-forum",
+        default="119_air_source_heat_pumps_ashp",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--forum",
+        help="forum (i.e. mse or bh)",
+        default="bh",
+        type=str,
+    )
+
+    args = parser.parse_args()
+    return args
 
 
-# Select the top N questions to display
-top_n = 5
-top_questions = question_counts.head(top_n)
+def load_data(file_path: str) -> pd.DataFrame:
+    """
+    Loads data from the extracted questions CSV file into a pandas DataFrame.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: The loaded data.
+    """
+    return pd.read_csv(file_path)
 
 
-# Function to wrap text and add an ellipsis after the first couple of lines
-def wrap_text_with_ellipsis(text, line_width, max_lines):
+def get_question_counts(df: pd.DataFrame, column_name: str = "Question") -> pd.Series:
+    """
+    Calculates the frequency of each unique question in a specified column.
+
+    Args:
+        df (pd.DataFrame): The DataFrame containing the questions.
+        column_name (str): The name of the column from which to count questions.
+
+    Returns:
+        pd.Series: A Series containing question counts, indexed by the question text.
+    """
+    return df[column_name].value_counts()
+
+
+def wrap_text_with_ellipsis(text: str, line_width: int, max_lines: int) -> str:
+    """
+    Wraps text to a specified line width and maximum number of lines, adding an ellipsis if truncated.
+
+    Args:
+        text (str): The text to wrap.
+        line_width (int): The maximum width of each line.
+        max_lines (int): The maximum number of lines.
+
+    Returns:
+        str: The wrapped (and possibly truncated) text with an ellipsis if necessary.
+    """
     wrapped_text = textwrap.wrap(text, width=line_width)
     if len(wrapped_text) > max_lines:
         return "\n".join(wrapped_text[:max_lines]) + "..."
@@ -40,25 +105,51 @@ def wrap_text_with_ellipsis(text, line_width, max_lines):
         return "\n".join(wrapped_text)
 
 
-# Wrap text for each question and add ellipsis if it's longer than 2 lines
-wrapped_questions = [wrap_text_with_ellipsis(q, 40, 2) for q in top_questions.index]
+def plot_question_counts(
+    question_counts: pd.Series, top_n: int = 5, figure_path: str = None
+) -> None:
+    """
+    Plots a horizontal bar chart of the most frequent questions.
 
-# Increase figure size for better visibility
+    Args:
+        question_counts (pd.Series): A Series containing question counts, indexed by question text.
+        top_n (int): The number of top questions to display.
+    """
+    top_questions = question_counts.head(top_n)
+    wrapped_questions = [wrap_text_with_ellipsis(q, 40, 2) for q in top_questions.index]
 
-plt.figure(figsize=(12, 8))  # Adjust the width as necessary
+    plt.figure(figsize=(12, 8))
+    plt.barh(wrapped_questions, top_questions.values, color=NESTA_COLOURS[0])
+    plt.gcf().subplots_adjust(left=0.5)
+    plt.xlabel("Frequency")
+    plt.ylabel("Questions")
+    plt.title(f"Top {top_n} Most Frequent Questions")
+    plt.gca().invert_yaxis()
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.savefig(
+        figure_path + "most_frequent_questions.png", dpi=300, bbox_inches="tight"
+    )
+    plt.show()
 
-# Plotting
-plt.barh(wrapped_questions, top_questions.values, color=NESTA_COLOURS[0])
-# Increase left margin to fit the wrapped questions
-plt.gcf().subplots_adjust(left=0.5)
-plt.xlabel("Frequency")
-plt.ylabel("Questions")
-plt.title(f"Top {top_n} Most Frequent Questions")
-plt.gca().invert_yaxis()  # To display the highest value at the top
 
-# Set x-axis major locator to integer values
-plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+def main():
+    """
+    Main function to perform data loading, question counting, and plotting the most frequent questions.
+    """
+    args = create_argparser()
+    category = args.category
+    forum = args.forum
+    input_data = os.path.join(
+        PROJECT_DIR,
+        f"outputs/data/extracted_questions/{forum}/forum_{category}/extracted_questions_{category}_all.csv",
+    )
+    output_figures_path = os.path.join(
+        PROJECT_DIR, f"outputs/figures/extracted_questions/{forum}/forum_{category}/"
+    )
+    extracted_questions_df = load_data(input_data)
+    question_counts = get_question_counts(extracted_questions_df)
+    plot_question_counts(question_counts, figure_path=output_figures_path)
 
-# Show plot with wrapped text for y-axis labels
-# plt.tight_layout()  # Adjust layout to make room for the wrapped question text
-plt.show()
+
+if __name__ == "__main__":
+    main()
