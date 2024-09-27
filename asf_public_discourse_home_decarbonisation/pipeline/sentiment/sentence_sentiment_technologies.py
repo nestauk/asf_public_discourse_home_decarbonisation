a3973945bb8f5bf502c32a2cc36d95cdad388482
sofiapinto <anasbpinto3@gmail.com>
2024-09-27 14:05:17 +0100

    adapted refactoring suggestion from aidan

--- asf_public_discourse_home_decarbonisation/pipeline/sentiment/sentence_sentiment_technologies.py
+++ asf_public_discourse_home_decarbonisation/pipeline/sentiment/sentence_sentiment_technologies.py
@@ -15,6 +15,9 @@ python asf_public_discourse_home_decarbonisation/pipeline/sentiment/sentence_sen
 from tqdm import tqdm
 import pandas as pd
 from argparse import ArgumentParser
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Local imports
 from asf_public_discourse_home_decarbonisation.getters.getter_utils import (
@@ -53,6 +56,64 @@ def parse_arguments(parser):
     return parser.parse_args()
 
 
+def compute_sentiment(sentences_data: list, chunk_size: int) -> pd.DataFrame:
+    """
+    Calls the SentenceBasedSentiment class to compute sentiment for a list of sentences.
+    The sentences are processed in chunks of size chunk_size.
+
+    Args:
+        sentences_data (list): sentences data
+        chunk_size (int): chunk size
+
+    Returns:
+        pd.DataFrame: dataframe with sentiment original sentences, sentiment and score
+    """
+    sentiment_model = SentenceBasedSentiment()
+    all_sentiment = []
+    for text in tqdm(list_chunks(sentences_data, chunk_size=chunk_size)):
+        sentiment_scores = sentiment_model.get_sentence_sentiment(text)
+        all_sentiment += sentiment_scores
+
+    all_sentiment = pd.DataFrame(all_sentiment, columns=["text", "sentiment", "score"])
+
+    return all_sentiment
+
+
+def prep_data_for_sentiment_model(
+    mse_data: pd.DataFrame, tech: str, start_date: str, end_date: str, test: bool
+) -> list:
+    """
+    Prepares the data for sentiment analysis by extracting sentences containing mentions of the technology
+    and removing duplicates.
+
+    Args:
+        mse_data (pd.DataFrame): dataframe with mse data
+        tech (str): technology e.g. "heat pump"
+        start_date (str): start date of the analysis in the format YYYY-MM-DD
+        end_date (str): end date of the analysis in the format YYYY-MM-DD
+        test (bool): True for test mode
+
+    Returns:
+        list: list of sentences
+    """
+    sentences_data = prepping_data_for_topic_analysis(
+        mse_data,
+        tech,
+        start_date,
+        end_date,
+        only_keep_sentences_with_expression=True,
+    )
+
+    if test:
+        sentences_data = sentences_data.head(50)
+
+    sentences_data = list(sentences_data.drop_duplicates("sentences")["sentences"])
+
+    logger.info(f"Number of unique sentences for {tech} tech: {len(sentences_data)}")
+
+    return sentences_data
+
+
 if __name__ == "__main__":
     chunk_size = 100
     parser = ArgumentParser()
@@ -62,101 +123,31 @@ if __name__ == "__main__":
         category="all", collection_date=config["latest_data_collection_date"]["mse"]
     )
 
-    hp_sentences_data = prepping_data_for_topic_analysis(
-        mse_data,
-        "heat pump",
-        args.start_date,
-        args.end_date,
-        only_keep_sentences_with_expression=True,
-    )
-
-    solar_sentences_data = prepping_data_for_topic_analysis(
-        mse_data,
-        "solar panel",
-        args.start_date,
-        args.end_date,
-        only_keep_sentences_with_expression=True,
-    )
-
-    boilers_sentences_data = prepping_data_for_topic_analysis(
-        mse_data,
-        "boiler",
-        args.start_date,
-        args.end_date,
-        only_keep_sentences_with_expression=True,
-    )
-
-    if args.test:
-        hp_sentences_data = hp_sentences_data.head(50)
-        solar_sentences_data = solar_sentences_data.head(50)
-        boilers_sentences_data = boilers_sentences_data.head(50)
-
-    hp_sentences_data = list(
-        hp_sentences_data.drop_duplicates("sentences")["sentences"]
-    )
-    solar_sentences_data = list(
-        solar_sentences_data.drop_duplicates("sentences")["sentences"]
-    )
-    boilers_sentences_data = list(
-        boilers_sentences_data.drop_duplicates("sentences")["sentences"]
-    )
-
-    sentiment_model = SentenceBasedSentiment()
-
-    # BOILERS
-    all_sentiment = []
-    for text in tqdm(list_chunks(boilers_sentences_data, chunk_size=chunk_size)):
-        sentiment_scores = sentiment_model.get_sentence_sentiment(text)
-        all_sentiment += sentiment_scores
-
-    all_sentiment = pd.DataFrame(all_sentiment, columns=["text", "sentiment", "score"])
-
-    print(all_sentiment.head())
-
     path_to_save_prefix = f"data/mse/outputs/sentiment/comparing_technologies/{args.start_date}_{args.end_date}"
-    output_name = f"{path_to_save_prefix}_mse_boiler_sentences_sentiment.csv"
 
-    if not args.test:
-        save_to_s3(
-            S3_BUCKET,
-            all_sentiment,
-            output_name,
+    technologies = ["heat pump", "solar panel", "boiler"]
+
+    for tech in technologies:
+        logger.info(f"Computing sentiment for {tech} sentences.")
+        sentences_data = prep_data_for_sentiment_model(
+            mse_data=mse_data,
+            tech=tech,
+            start_date=args.start_date,
+            end_date=args.end_date,
+            test=args.test,
         )
 
-    # SOLAR PANELS
-    all_sentiment = []
-    for text in tqdm(list_chunks(solar_sentences_data, chunk_size=chunk_size)):
-        sentiment_scores = sentiment_model.get_sentence_sentiment(text)
-        all_sentiment += sentiment_scores
+        all_sentiment = compute_sentiment(sentences_data, chunk_size)
 
-    all_sentiment = pd.DataFrame(all_sentiment, columns=["text", "sentiment", "score"])
-
-    print(all_sentiment.head())
-
-    output_name = f"{path_to_save_prefix}_mse_solar_panel_sentences_sentiment.csv"
-
-    if not args.test:
-        save_to_s3(
-            S3_BUCKET,
-            all_sentiment,
-            output_name,
+        tech_filename = tech.replace(" ", "_")
+        output_name = (
+            f"{path_to_save_prefix}_mse_{tech_filename}_sentences_sentiment.csv"
         )
 
-    # Heat pumps
-    all_sentiment = []
-    for text in tqdm(list_chunks(hp_sentences_data, chunk_size=chunk_size)):
-        sentiment_scores = sentiment_model.get_sentence_sentiment(text)
-        all_sentiment += sentiment_scores
-
-    all_sentiment = pd.DataFrame(all_sentiment, columns=["text", "sentiment", "score"])
-
-    print(all_sentiment.head())
-
-    output_name = f"{path_to_save_prefix}_mse_heat_pump_sentences_sentiment.csv"
-
-    if not args.test:
-        save_to_s3(
-            S3_BUCKET,
-            all_sentiment,
-            output_name,
-        )
+        if not args.test:
+            save_to_s3(
+                S3_BUCKET,
+                all_sentiment,
+                output_name,
+            )
+        logger.info(f"Sentiment for {tech} sentences completed.")
